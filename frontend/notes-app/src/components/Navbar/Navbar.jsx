4b9a378f3643830a8bedcfa23f4db18dc69ff652
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2024-05-11 01:31:49 +0530

    signup complete and added profile and search to nav

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import ProfileInfo from '../Cards/ProfileInfo'
+import { useNavigate } from 'react-router-dom'
+import SearchBar from '../SearchBar/SearchBar';
 
 const Navbar = () => {
+  const navigate=useNavigate;
+
+  const onLogout=()=>{
+     navigate("/login")
+  }
+
   return (
     <div className=' bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
       <h2 className=' text-xl font-medium text-black py-2'>Notes</h2>
-
+      <SearchBar/>
+      <ProfileInfo onLogout={onLogout} />
     </div>
   )
 }
