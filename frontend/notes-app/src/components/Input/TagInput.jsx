9c8daffd1ad2d86171d6faae01953299fa063e9b
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2024-05-26 23:41:03 +0530

    working on input tag :construction:

--- frontend/notes-app/src/components/Input/TagInput.jsx
+++ frontend/notes-app/src/components/Input/TagInput.jsx
@@ -2,45 +2,44 @@ import React, { useState } from 'react'
 import { MdAdd, MdClose } from 'react-icons/md'
 
 const TagInput = ({ tags, setTags }) => {
+
     const [inputValue, setInputValue] = useState("")
+
     const handleInputChange = (e) => {
         setInputValue(e.target.value)
     }
+
     const addNewTag = () => {
         if (inputValue.trim() !== "") {
             setTags([...tags, inputValue.trim()])
             setInputValue("")
         }
     }
-
     const handleKeyDown = (e) => {
         if (e.key === "Enter") {
             addNewTag()
         }
     }
 
-    const handleRemovetag=(tagToRemove)=>{
-        setTags(tags.filter((tag)=>tag!==tagToRemove))
+    const handleRemoveTag=(tagToRemove)=>{
+        setTags(tags.filter((tag)=>tag !== tagToRemove))
     }
     return (
         <div>
-
-            {tags?.length>0 && (
-            <div className=" flex items-center gap-2 flex-wrap mt-2">
-                {tags.map((tag, index) => (
-                    <span key={index} className='' >#{tag}
-                        <button onClick={() => { handleRemovetag(tag)}}>
-                            <MdClose />
-                        </button>
-                    </span>
-                ))}
-            </div>)}
+            {tags.length > 0 &&
+                (<div className="flex items-center gap-2 flex-wrap mt-2">
+                    {tags.map((tag, index) => (
+                        <span className='' key={index}>
+                            #{tag}
+                            <button onClick={() => { handleRemoveTag(tag)}}>
+                                <MdClose />
+                            </button>
+                        </span>
+                    ))}
+                </div>)}
             <div className=" flex items-center gap-4 mt-3">
                 <input type="text" className=' text-sm bg-transparent border px-3 py-2 rounded outline-none' placeholder='Add Tags' onChange={handleInputChange} onKeyDown={handleKeyDown} />
-
-                <button className='flex w-8 h-8 items-center justify-center rounded border border-blue-700 hover:bg-blue-700' onClick={() => {
-                    addNewTag()
-                }}>
+                <button className='flex w-8 h-8 items-center justify-center rounded border border-blue-700 hover:bg-blue-700' onClick={() => { addNewTag() }}>
                     <MdAdd className=' text-2xl text-blue-700 hover:text-white' />
                 </button>
             </div>
