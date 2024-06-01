bb29d3660ad067a32a5467ca35e48422bab4fa00
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2024-06-01 23:14:56 +0530

    login api :construction:

--- backend/index.js
+++ backend/index.js
@@ -75,8 +75,28 @@ app.post("/login",async(req,res)=>{
     }
 
     const userInfo=await User.findOne({email:email})
+    if (!userInfo){
+        return res.status(400).json({message:"User not found"})
 
-    
+    }
+    if(userInfo.email==email && userInfo.password==password){
+        const user ={user:userInfo}
+        const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn:"36000m"
+        })
+        return res.json({
+            error:false,
+            message:"Login Successful",
+            email,
+            accessToken
+        })
+    }
+    else{
+        return res.status(400).json({
+            error:true,
+            message:"Invalid Credentials"
+        })
+    }
 })
 
 app.listen(8000)
