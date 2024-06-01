0e29e0a45a1ed8104db902c2f2d6164c28d1931a
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2024-06-02 01:11:18 +0530

    add note api made :sparkles:

--- backend/index.js
+++ backend/index.js
@@ -1,50 +1,51 @@
 require("dotenv").config()
 
-const config=require("./config.json")
-const mongoose=require("mongoose")
+const config = require("./config.json")
+const mongoose = require("mongoose")
 
 mongoose.connect(config.connectionString)
 
-const User=require("./models/user.model")
-const express =require("express")
-const cors=require("cors")
-const app=express()
+const User = require("./models/user.model")
+const Note = require("./models/note.model")
+const express = require("express")
+const cors = require("cors")
+const app = express()
 
-const jwt =require("jsonwebtoken")
-const {authenticateToken}=require("./utilities")
+const jwt = require("jsonwebtoken")
+const { authenticateToken } = require("./utilities")
 
 app.use(express.json())
 
 app.use(
     cors({
-        origin:"*"
+        origin: "*"
     })
 )
 
-app.get("/",(req,res)=>{
-    res.json({data:"hello"})
+app.get("/", (req, res) => {
+    res.json({ data: "hello" })
 })
 
-app.post("/create-account",async (req,res)=>{
-    const {fullName,email,password}=req.body
+app.post("/create-account", async (req, res) => {
+    const { fullName, email, password } = req.body
 
-    if(!fullName){
-        return res.status(400).json({error:true,message:"Full Name is required"})
+    if (!fullName) {
+        return res.status(400).json({ error: true, message: "Full Name is required" })
     }
-    if(!email){
-        return res.status(400).json({error:true,message:"Email is required"})
+    if (!email) {
+        return res.status(400).json({ error: true, message: "Email is required" })
     }
-    if(!password){
-        return res.status(400).json({error:true,message:"Password is required"})
-    }
-
-    const isUser=await User.findOne({email:email})
-
-    if(isUser){
-        return res.json({error:true,message:"User already exist"})
+    if (!password) {
+        return res.status(400).json({ error: true, message: "Password is required" })
     }
 
-    const user=new User({
+    const isUser = await User.findOne({ email: email })
+
+    if (isUser) {
+        return res.json({ error: true, message: "User already exist" })
+    }
+
+    const user = new User({
         fullName,
         email,
         password
@@ -52,52 +53,85 @@ app.post("/create-account",async (req,res)=>{
 
     await user.save()
 
-    const accessToken=jwt.sign({user},process.env.ACCESS_TOKEN_SECRET,{
-        expiresIn:"36000m"
+    const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "36000m"
     })
 
     return res.json({
-        error:false,
+        error: false,
         user,
         accessToken,
-        message:"Registration Succesfull"
+        message: "Registration Succesfull"
     })
-    
+
 })
 
-app.post("/login",async(req,res)=>{
-    const {email,password}=req.body
-    if(!email){
-        return res.status(400).json({message:"Email is required"})
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body
+    if (!email) {
+        return res.status(400).json({ message: "Email is required" })
     }
-    if(!password){
-        return res.status(400).json({message:"Password is required"})
+    if (!password) {
+        return res.status(400).json({ message: "Password is required" })
     }
 
-    const userInfo=await User.findOne({email:email})
-    if (!userInfo){
-        return res.status(400).json({message:"User not found"})
+    const userInfo = await User.findOne({ email: email })
+    if (!userInfo) {
+        return res.status(400).json({ message: "User not found" })
 
     }
-    if(userInfo.email==email && userInfo.password==password){
-        const user ={user:userInfo}
-        const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
-            expiresIn:"36000m"
+    if (userInfo.email == email && userInfo.password == password) {
+        const user = { user: userInfo }
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "36000m"
         })
         return res.json({
-            error:false,
-            message:"Login Successful",
+            error: false,
+            message: "Login Successful",
             email,
             accessToken
         })
     }
-    else{
+    else {
         return res.status(400).json({
-            error:true,
-            message:"Invalid Credentials"
+            error: true,
+            message: "Invalid Credentials"
         })
     }
 })
 
+//add note
+app.post("/add-note", authenticateToken, async (req, res) => {
+    const { title, content, tags } = req.body
+    const { user } = req.user
+
+    if (!title) {
+        return res.status(400).json({error:true, message: "Title is required" })
+    }
+    if (!content) {
+        return res.status(400).json({error:true, message: "Content is required" })
+    }
+    try{
+        const note=new Note({
+            title,
+            content,
+            tags:tags||[],
+            userId:user._id
+        })
+
+        await note.save()
+
+        return res.json({
+            error:false,
+            note,
+            message:"Note added successfully"
+        })
+    }catch (error){
+        return res.status(500).json({
+            error:true,
+            message:"Internal Server Error"
+        })
+    }
+})
 app.listen(8000)
-module.exports=app
+module.exports = app
