ecd31aae1e083091b614af1b7643992dfc64119e
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2024-06-04 01:37:59 +0530

    pinned update api

--- backend/index.js
+++ backend/index.js
@@ -100,6 +100,8 @@ app.post("/login", async (req, res) => {
     }
 })
 
+
+
 //add note
 app.post("/add-note", authenticateToken, async (req, res) => {
     const { title, content, tags } = req.body
@@ -152,7 +154,9 @@ app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
         if (title) note.title = title
         if (content) note.content = content
         if (tags) note.tags = tags
-        if (isPinned) note.isPinned = isPinned
+        if (typeof isPinned !== 'undefined') {
+            note.isPinned = isPinned;
+        }
 
         await note.save()
 
@@ -214,6 +218,39 @@ app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
             message: "Internal Server Error"
         })
     }
+
+})
+
+//update isPinned value
+app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
+    const noteId = req.params.noteId
+    const {user}=req.user
+    const {isPinned}=req.body
+
+    try {
+        const note = await Note.findOne({ _id: noteId, userId: user._id })
+
+        if (!note) {
+            return res.status(404).json({ error: true, message: "Note not found" })
+        }
+        
+        if (typeof isPinned !== 'undefined') {
+            note.isPinned = isPinned;
+        }
+
+        await note.save()
+
+        return res.json({ error: false, note, message: "Pinned updated succesfullly" })
+
+    }
+    catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: "Internal Server Error"
+        })
+    }
+
+
 
 })
 app.listen(8000)
