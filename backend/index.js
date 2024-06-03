01e788284781ab909522ad9bb25af32857afa26d
Shrutika Shaw <shrutika.shaw2015@gmail.com>
2024-06-03 20:38:27 +0530

    delete note api made

--- backend/index.js
+++ backend/index.js
@@ -170,23 +170,51 @@ app.put("/edit-note/:noteId", authenticateToken, async (req, res) => {
 
 //get all notes
 app.get("/get-all-notes/", authenticateToken, async (req, res) => {
-    const {user}=req.user
+    const { user } = req.user
 
-    try{
-        const notes=await Note.find({userId:user._id}).sort({isPinned:-1})
+    try {
+        const notes = await Note.find({ userId: user._id }).sort({ isPinned: -1 })
 
         return res.json({
-            error :false,
+            error: false,
             notes,
-            message:"All notes retrived successfully"
+            message: "All notes retrived successfully"
         })
     }
-    catch(error){
+    catch (error) {
         return res.status(500).json({
             error: true,
             message: "Internal Server Error"
         })
     }
+})
+
+//delete note
+app.delete("/delete-note/:noteId", authenticateToken, async (req, res) => {
+    const noteId = req.params.noteId
+    const { user } = req.user
+
+    try {
+        const note = await Note.findOne({ _id: noteId, userId: user._id })
+
+        if (!note) {
+            return res.status(404).json({ error: true, message: "Note not found" })
+        }
+        await Note.deleteOne({ _id: noteId, userId: user._id })
+
+        return res.json({
+            error: false,
+            message: "Note deleted succesfully"
+        })
+
+    }
+    catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: "Internal Server Error"
+        })
+    }
+
 })
 app.listen(8000)
 module.exports = app
